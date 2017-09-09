abe5900362c74234c4cff93239e6e7566cd2606d
Andrey Dubov <icewinter8@gmail.com>
2017-09-09 13:59:34 +0400

    Initial Commit

---
+++ project02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project02
+//
+//  Created by Andrey Dubov on 09.09.17.
+//  Copyright © 2017 Andrey Dubov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
